a1da378d28ba93af2227a38cff4cafc914cc69dc
mbusson <maxencebusson@gmail.com>
2016-04-21 13:56:09 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Maxence Busson | Front-End Developer</title>
+	 <link rel="stylesheet" href="styles.css">
+	 <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+	<div id="logo">
+		<h1>Maxence Busson</h1>
+	</div>
+	<header>
+		<h2>Front-End Web Development</h2>
+		<div id="contact">
+			<i class="fa fa-envelope-o"></i>
+		</div>
+	</header>
+	
+	<div id="middle">
+	</div>
+	
+	<nav>
+		<div class="menu">Projet 1</div>
+		<div class="menu">Projet 2</div>
+		<div class="menu">Projet 3</div>
+		<div class="menuret"> </div>
+	</nav>
+	
+	<section id="bio">
+	<p>Bio</p>
+	</section>
+	
+	<footer>
+	</footer>
+
+</body>
+</html>
